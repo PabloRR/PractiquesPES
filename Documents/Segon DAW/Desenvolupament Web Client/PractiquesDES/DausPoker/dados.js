7f7542fea1b3e62a322c6ce47894eaad7270d37d
Carlos <jordi@pepa.com>
2013-10-18 19:52:24 +0200

    First commit des

---
+++ Documents/Segon DAW/Desenvolupament Web Client/PractiquesDES/DausPoker/dados.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+function tirarDau()
+{
+    var dau;
+    dau = Math.floor((Math.random()*6)+1);
+    alert(dau);
+}
